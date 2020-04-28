fe5b1f3b51bdacb245cedf68fa780d667671c3f0
Vasudev <vasudevburada07@gmail.com>
2020-04-28 14:16:12 +0530

    Fixed cross origin issues

--- CustomerManagement/src/main/java/com/springoot/CustomerManagementApplication.java
+++ CustomerManagement/src/main/java/com/springoot/CustomerManagementApplication.java
@@ -2,12 +2,25 @@ package com.springoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CustomerManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerManagementApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
--- CustomerManagement/src/main/java/com/springoot/controller/EmployeeController.java
+++ CustomerManagement/src/main/java/com/springoot/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.springoot.repo.EmployeeRepository;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
